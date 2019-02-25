6d2da5ff307e7b025ae32b3e961f361c970a8eb1
Paul <paul.raestad@gmail.com>
2019-02-25 17:14:17 +0100

    Kartlegging av vinkler

--- main.cpp
+++ main.cpp
@@ -5,18 +5,46 @@
 using namespace Iderwok;
 
 int main(int argc, char** argv) {
-	
-	Vec2d<double> sp{15.0, 15.0};
+	Vec2d<double> x_axe{150.0, 0.0};
+	std::cout << "x_axe:\t" << x_axe << std::endl; 
+	Vec2d<double> sp{15.0, -15.0};
 	std::cout << "sp:\t" << sp << std::endl; 
-	Vec2d<double> v1{10.0,30.0};
-	std::cout << "v1:\t" << v1 << std::endl;
+	std::cout << "Vinkel mellom sp og x_axe = " << angle_deg(sp, x_axe) << std::endl;
+	std::cout << " Rad vinkel mellom sp og x_axe = " << angle_rad(sp, x_axe) << std::endl;
+	
+	sp = {15.0, 15.0};
+	std::cout << "\nsp:\t" << sp << std::endl; 
+	std::cout << "Vinkel mellom sp og x_axe = " << angle_deg(sp, x_axe) << std::endl;
+	std::cout << " Rad vinkel mellom sp og x_axe = " << angle_rad(sp, x_axe) << std::endl;
+	
+	sp = {-15.0, 15.0};
+	std::cout << "\nsp:\t" << sp << std::endl; 
+	std::cout << "Vinkel mellom sp og x_axe = " << angle_deg(sp, x_axe) << std::endl;
+	std::cout << " Rad vinkel mellom sp og x_axe = " << angle_rad(sp, x_axe) << std::endl;
+	
+	sp = {-15.0, -15.0};
+	std::cout << "\nsp:\t" << sp << std::endl; 
+	std::cout << "Vinkel mellom sp og x_axe = " << angle_deg(sp, x_axe) << std::endl;
+	std::cout << " Rad vinkel mellom sp og x_axe = " << angle_rad(sp, x_axe) << std::endl;
+	
+
+//	Vec2d<double> v1{10.0,30.0};
+//	std::cout << "v1:\t" << v1 << std::endl;
 //	Vec2d<double> v2{2.0, 0.0};
 //	std::cout << "v2:\t" << v2 << std::endl;
 //	Vec2d<double> v3{v1 + v2};
 //	std::cout << "v3:\t" << v3 << std::endl;
-	Vec2d<double> spv3{v1-sp};
-	std::cout << "spv3:\t" << spv3 << std::endl;
+//	Vec2d<double> spv3{v1-sp};
+//	std::cout << "spv3:\t" << spv3 << std::endl;
 	
 //	Vec2d<double> v3
 	return 0;
 }
+
+
+//				
+//				              | 
+//		[-15, -15] ->	      |		[15,-15] -> 
+//		[-15, 0] ->		------------ [15, 0] ->
+//		[-15, 15] ->		     |[15, 15] ->
+//				     |
